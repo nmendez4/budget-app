4fde01694c285bc77c785e3c17ee649bb1a96c49
nmendez4 <nmendez6594@gmail.com>
2022-02-13 11:12:41 -0500

    added service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+const APP_PREFIX = 'Budget-Tracker';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x1552.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cachesKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cachesKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cachesKeepList.indexOf(key) === -1) {
+                    console.log('deleting caches: ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond w/ cache
+                console.log('responding with cache : ' + e.request.url);
+                return request;
+            } else { // if there is no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request);
+            }
+        })
+    )
+});
